91b4e2753c80e0668bcdc0d69e6bf35ef35e3ee1
DeliriumCuriosity <100794261+DeliriumCuriosity@users.noreply.github.com>
2023-11-25 23:01:43 +0300

    Initial commit

---
+++ src/main/java/org/example/Human.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class Human {
+    String firstName;
+    String lastName;
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human = new Human("Andrey", "Andreev");
+        System.out.println(human);
+    }
+}
